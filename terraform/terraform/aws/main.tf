efe6e43aeeb1fa305734339035362e053f742887
Joseph Isaac <joe2kool@users.noreply.github.com>
2026-01-29 16:06:27 -0500

    Add DynamoDB state locking and harden Terraform backend

--- terraform/terraform/aws/main.tf
+++ terraform/terraform/aws/main.tf
@@ -1,8 +1,11 @@
 terraform {
   backend "s3" {
-    bucket = "jisaac-homelab-tf-state"
-    key    = "aws/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "jisaac-homelab-tf-state"
+    key            = "aws/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+
   }
 
   required_providers {
@@ -47,3 +50,15 @@ resource "aws_s3_bucket_public_access_block" "tf_state" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+
+resource "aws_dynamodb_table" "tf_locks" {
+  name         = "terraform-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}  
